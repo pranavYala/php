7608795576797d022044ff48e76b619f890bce0c
pranavYala <94856672+pranavYala@users.noreply.github.com>
2021-11-24 14:12:34 -0500

    Add files via upload

---
+++ post_form.php
@@ -0,0 +1,12 @@
+<html>
+<h2>Post Form</h2>
+<form action="post.php" method="POST">
+  <label for="title">Title:</label><br>
+  <input type="text" id="title" name="title"><br>
+  <label for="cont">Content:</label><br>
+  <input type="text" id="cont" name="cont"><br>
+  <label for="desc">Description:</label><br>
+  <input type="text" id="desc" name="desc"><br><br>
+  <button type="submit">Submit</button>
+</form>
+</html>
